8d6cc8daac268668162472d1827b8ef3b5a53090
Jesse Bounds <jesse@rebounds.net>
2012-03-31 09:14:13 -0700

    trying fix for pub/sub issue on heroku

--- tstream.js
+++ tstream.js
@@ -15,7 +15,25 @@ function init(redisUrl) {
 
 	console.log("attempting connetion to redis with " + redisUrl);
 
-	var redis = require('redis-url').createClient(redisUrl);
+    var url = require('url');
+    var parsedUrl  = url.parse(redisUrl);
+    var parsedAuth = (parsedUrl.auth || '').split(':');
+    console.log('parsed url host: ' + parsedUrl.host);
+    console.log('parsed url port: ' + parsedUrl.port);
+    console.log('parsed url auth: ' + parsedAuth);
+
+	//var redis = require('redis-url').createClient(redisUrl);
+  
+    var redis = require('redis').createClient(parsedUrl.port, parsedUrl.hostname); 
+    var redisAuth = function() { 
+        if (redisAuth) {
+            redis.auth(parsedAuth); 
+        }
+    }
+
+	//redis.addListener('connected', redisAuth);
+	//redis.addListener('reconnected', redisAuth);
+	//redisAuth();
 
 	redis.on("message", function (channel, message) {
 		process.send(message);
